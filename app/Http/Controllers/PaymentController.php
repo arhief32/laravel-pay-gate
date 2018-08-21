afdf0771a447608a2aeace2f7728fd65ebf975fe
Arief Muarrief <arhief32@gmail.com>
2018-08-21 07:31:47 +0700

    create payment error code

--- app/Http/Controllers/PaymentController.php
+++ app/Http/Controllers/PaymentController.php
@@ -10,27 +10,108 @@ class PaymentController extends Controller
 {
     public function requestPayment(Request $request)
     {
-        $briva_number = substr($request->input('BrivaNum'), 0,5);
+        if($request->input('BrivaNum') == null)
+        {
+            return response()->json([
+                'isError' => '1',
+                'errorCode' => '01',
+                'errorDesc' => 'No Tagihan harus ada'    
+            ]);
+        }
+        if($request->input('IdApp') == null)
+        {
+            return response()->json([
+                'isError' => '1',
+                'errorCode' => '13',
+                'errorDesc' => 'ID User aplikasi harus ada'    
+            ]);
+        }
+        if($request->input('PassApp') == null)
+        {
+            return response()->json([
+                'isError' => '1',
+                'errorCode' => '14',
+                'errorDesc' => 'Password aplikasi harus ada'    
+            ]);
+        }
+        else
+        {
+            $briva_number = substr($request->input('BrivaNum'), 0,5);
+            $mapping = Mapping::where('corp_code', $briva_number)->first();
+            
+            if($mapping == null)
+            {
+                return response()->json([
+                    'isError' => '1',
+                    'errorCode' => '12',
+                    'errorDesc' => 'Koneksi ke API gagal'    
+                ]);
+            }
+            else
+            {
+                $client = new \GuzzleHttp\Client();
+                
+                try {
+                    $getUser = $client->request('GET', $mapping->url.'/get-user-detail/'.$request->input('BrivaNum'), 
+                        array(
+                            'timeout' => 15, // Response timeout
+                            'connect_timeout' => 15, // Connection timeout
+                        ))->getBody()->getContents();
+                    $user = json_decode($getUser);
+                } 
+                catch (\Exception $e) 
+                {
+                    return response()->json([
+                        'isError' => '1',
+                        'errorCode' => '10',
+                        'errorDesc' => 'Koneksi Timeout'    
+                    ]);
+                }
 
-        $mapping = Mapping::where('corp_code', $briva_number)->first();
+                if(http_response_code(500))
+                {
+                    return response()->json([
+                        'isError' => '1',
+                        'errorCode' => '21',
+                        'errorDesc' => 'Error query di database'    
+                    ]);
+                }
+                if(!isset($user))
+                {
+                    return response()->json([
+                        'isError' => '1',
+                        'errorCode' => '99',
+                        'errorDesc' => 'Undefined'    
+                    ]);
+                }
 
-        $client = new \GuzzleHttp\Client();
-        $getUser = $client->request('GET', $mapping->url.'/get-user-detail/'.$request->input('BrivaNum'))->getBody();
-        $user = json_decode($getUser);
-        
-        $statusPayment = [
-            'isError' => '0',
-            'errorCode' => $user->status->error_code,
-            'errorDesc' => $user->status->description
-        ];
-        
-        return response()->json([
-            'StatusPayment' => $statusPayment,
-            'Info1' => $user->info_1,
-            'Info2' => $user->info_2,
-            'Info3' => $user->info_3,
-            'Info4' => $user->info_4,
-            'Info5' => $user->info_5
-        ]);
+                if(!isset($user->id))
+                {
+                    return response()->json([
+                        'isError' => '1',
+                        'errorCode' => '14',
+                        'errorDesc' => 'Data tagihan tidak ditemukan'    
+                    ]);
+                }
+                else
+                {
+                    $statusPayment = [
+                        'isError' => '0',
+                        'errorCode' => '00',
+                        'errorDesc' => 'Sukses'
+                    ];
+                    
+                    return response()->json([
+                        'StatusPayment' => $statusPayment,
+                        'Info1' => $user->info_1,
+                        'Info2' => $user->info_2,
+                        'Info3' => $user->info_3,
+                        'Info4' => $user->info_4,
+                        'Info5' => $user->info_5
+                    ]);
+                }
+                
+            }
+        }
     }
 }
