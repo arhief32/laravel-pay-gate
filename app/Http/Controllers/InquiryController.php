29cf564d2655925fde794162147fde9e36b8af72
Arief Muarrief <arhief32@gmail.com>
2018-08-31 09:17:52 +0700

    repair function inquiry and payment

--- app/Http/Controllers/InquiryController.php
+++ app/Http/Controllers/InquiryController.php
@@ -9,14 +9,57 @@ class InquiryController extends Controller
 {
     public function requestInquiry(Request $request)
     {
-        $briva_number = substr($request->BrivaNum, 0,5);
+        $briva_number = substr($request->brivaNo, 0,5);
 
         $mapping = Mapping::where('corp_code', $briva_number)->first();
         
         $client = new \GuzzleHttp\Client();
-        $getInquiry = $client->request('GET', $mapping->corp_url.'request-inquiry?BrivaNum='.$request->BrivaNum)->getBody();
-        $inquiry = json_decode($getInquiry);
+        $getInquiry = $client->request('GET', $mapping->corp_url.'request-inquiry?BrivaNum='.$request->brivaNo)->getBody();
+        $inquiries = json_decode($getInquiry);
 
-        return response()->json($inquiry);
+        if($inquiries == false)
+        {
+            return [
+                'responseCode' => '99',
+                'responseDesc' => 'BRIVA number not found',
+                'responseData' => (object)$inquiries
+            ];
+        }
+        else
+        {
+            // define custDetailInvoice object
+            $detail_invoice = [];
+    
+            foreach ($inquiries as $inquiry)
+            {   
+                
+                array_push($detail_invoice, 
+                [
+                    'feeType' => $inquiry->feetype,
+                    'amount' => $inquiry->amount
+                ]);
+            }
+            
+            // define responseData object
+            $inquiry_amount_total = array();
+    
+            foreach ($inquiries as $inquiry)
+            {   
+                array_push($inquiry_amount_total, $inquiry->amount);
+            }
+    
+            $response_data = [
+                'custNo' => $inquiries[0]->registerNO,
+                'custName' => $inquiries[0]->name,
+                'custSumAmount' => array_sum($inquiry_amount_total),
+                'custDetailInvoice' => $detail_invoice
+            ];
+            
+            return response()->json([
+                'responseCode' => '00',
+                'responseDesc' => 'Inquiry Success',
+                'responseData' => $response_data
+            ]);
+        }
     }
 }
